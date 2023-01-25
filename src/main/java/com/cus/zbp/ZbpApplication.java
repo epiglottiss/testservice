9cfa12b9e5b01c896e95000ba5f0157d0e1708e1
의성 채 <iop245@naver.com>
2023-01-25 21:40:34 +0900

    first commit

---
+++ src/main/java/com/cus/zbp/ZbpApplication.java
@@ -0,0 +1,13 @@
+package com.cus.zbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cus/zbp/ZbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cus.zbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
